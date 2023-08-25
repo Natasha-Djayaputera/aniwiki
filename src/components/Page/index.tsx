6e6473ee7aac8c2c55ac5a92657298c84804219f
Natasha Djayaputera <natashadjayaputera@gmail.com>
2023-08-25 15:14:52 +0700

    Add more carousel in HomePage

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,9 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { TopService, anime, top_anime_filter } from "../../generated/jikan";
+import {
+  SeasonsService,
+  TopService,
+  anime,
+  top_anime_filter,
+} from "../../generated/jikan";
 import Carousel from "../Carousel";
 
 const Page: React.FunctionComponent = () => {
   const [topAiringAnime, setTopAiringAnime] = useState<anime[] | undefined>();
+  const [topUpcomingAnime, setTopUpcomingAnime] = useState<
+    anime[] | undefined
+  >();
+  const [currentSeasonAnime, setCurrentSeasonAnime] = useState<
+    anime[] | undefined
+  >();
 
   const getTopAiring = async () => {
     try {
@@ -20,8 +31,37 @@ const Page: React.FunctionComponent = () => {
     }
   };
 
+  const getCurrentSeason = async () => {
+    try {
+      const response = await SeasonsService.getSeasonNow();
+
+      console.log(response);
+      setCurrentSeasonAnime(response.data);
+      console.log(currentSeasonAnime);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getTopUpcoming = async () => {
+    try {
+      const response = await TopService.getTopAnime(
+        undefined,
+        top_anime_filter.UPCOMING
+      );
+
+      console.log(response);
+      setTopUpcomingAnime(response.data);
+      console.log(topUpcomingAnime);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   useEffect(() => {
     getTopAiring();
+    getCurrentSeason();
+    getTopUpcoming();
   }, []);
 
   return (
@@ -50,11 +90,11 @@ const Page: React.FunctionComponent = () => {
         </div>
         <div className="content-item">
           <h3>Current Season Anime</h3>
-          <div className="carousel"></div>
+          <Carousel itemData={currentSeasonAnime} />
         </div>
         <div className="content-item">
           <h3>Top Upcoming Anime</h3>
-          <div className="carousel"></div>
+          <Carousel itemData={topUpcomingAnime} />
         </div>
       </div>
     </main>
